3dc5b89523e5de2a3d8a0ffe1c18441561c41d0f
khaisendefo <khaisendefo@gmail.com>
2024-12-14 03:49:59 +0300

    Added gsap animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,71 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,  
+    effects: true
+});
+
+let panels = gsap.utils.toArray(".panel");  
+
+let tops = panels.map(panel => ScrollTrigger.create({trigger: panel, start: "top top"}));
+
+panels.forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: () => panel.offsetHeight < window.innerHeight ? "top top" : "bottom bottom", 
+    pin: true, 
+    pinSpacing: false 
+  });
+});
+
+
+
+
+
 const depositValue = document.getElementById('depositValue');
 const holdingValue = document.getElementById('holdingValue');
 const futureBalance = document.getElementById('futureBalance');
 const generatedAmount = document.getElementById('generatedAmount');
-const depositSlider = document.getElementById('depositSlider'); 
-const holdingSlider = document.getElementById('holdingSlider'); 
+const depositSlider = document.getElementById('depositSlider');
+const holdingSlider = document.getElementById('holdingSlider');
 
 function saveToLocalStorage() {
-  localStorage.setItem('deposit', depositSlider.value);
-  localStorage.setItem('holding', holdingSlider.value);
+    localStorage.setItem('deposit', depositSlider.value);
+    localStorage.setItem('holding', holdingSlider.value);
 }
 
 function loadFromLocalStorage() {
-  const savedDeposit = localStorage.getItem('deposit');
-  const savedHolding = localStorage.getItem('holding');
+    const savedDeposit = localStorage.getItem('deposit');
+    const savedHolding = localStorage.getItem('holding');
 
-  if (savedDeposit !== null) depositSlider.value = savedDeposit;
-  if (savedHolding !== null) holdingSlider.value = savedHolding;
+    if (savedDeposit !== null) depositSlider.value = savedDeposit;
+    if (savedHolding !== null) holdingSlider.value = savedHolding;
 }
 
 function updateCalculator() {
-  const deposit = parseFloat(depositSlider.value);
-  const holding = parseInt(holdingSlider.value, 10);
+    const deposit = parseFloat(depositSlider.value);
+    const holding = parseInt(holdingSlider.value, 10);
 
-  depositValue.textContent = deposit;
-  holdingValue.textContent = holding;
+    depositValue.textContent = deposit;
+    holdingValue.textContent = holding;
 
-  const futureBalanceValue = deposit * Math.pow(1.0067, holding);
-  const generatedAmountValue = futureBalanceValue - deposit;
+    const futureBalanceValue = deposit * Math.pow(1.0067, holding);
+    const generatedAmountValue = futureBalanceValue - deposit;
 
-  futureBalance.textContent = Math.round(futureBalanceValue);
-  generatedAmount.textContent = Math.round(generatedAmountValue); 
+    futureBalance.textContent = Math.round(futureBalanceValue);
+    generatedAmount.textContent = Math.round(generatedAmountValue);
 
-  saveToLocalStorage();
-  updateSliderProgress(); // Обновляем прогресс слайдера
+    saveToLocalStorage();
+    updateSliderProgress(); 
 }
 
 function updateSliderProgress() {
-  const sliderWidth = depositSlider.offsetWidth; // Получаем ширину слайдера
-  const value = depositSlider.value;
-  const percentage = (value / depositSlider.max) * 100;  // Рассчитываем процентное значение слайдера
-  depositSlider.style.setProperty('--slider-progress', `${percentage}%`); // Устанавливаем ширину для псевдо-элемента
+    const sliderWidth = depositSlider.offsetWidth; 
+    const value = depositSlider.value;
+    const percentage = (value / depositSlider.max) * 100; 
+    depositSlider.style.setProperty('--slider-progress', `${percentage}%`); 
 }
 
 loadFromLocalStorage();
@@ -47,35 +73,4 @@ loadFromLocalStorage();
 depositSlider.addEventListener('input', updateCalculator);
 holdingSlider.addEventListener('input', updateCalculator);
 
-updateCalculator(); // Инициализация значений при загрузке страницы
-
-
-/// CONNECT SmoothScroll
-SmoothScroll({ 
-  animationTime: 800, stepSize: 75, accelerationDelta: 30, accelerationMax: 2, keyboardSupport: true,
-  arrowScroll: 50, pulseAlgorithm: true, pulseScale: 4, pulseNormalize: 1, touchpadSupport: true,
-});
-
-// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-// ScrollSmoother.create({
-//     wrapper: '.wrapper',
-//     content: '.content',
-//     smooth: 1.5,  // Плавность скроллинга
-//     effects: true
-// });
-
-const tl = gsap.timeline();
-tl.fromTo('.wrapper-inner', {x: '0%', y: '0%'}, {y: '-100%'});
-
-const main = document.querySelector('.main');
-
-// Используем offsetHeight для правильного завершения анимации по высоте
-ScrollTrigger.create({
-    animation: tl,
-    trigger: '.wrapper',
-    start: 'top top',
-    // end: () => main.offsetHeight, // Используем offsetHeight, чтобы завершить анимацию по высоте
-    scrub: true,
-    pin: true
-});
+updateCalculator(); 
